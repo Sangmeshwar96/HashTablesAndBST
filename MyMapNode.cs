fc571c34d8b24b5fbbbbdca48f250f5e5eae5430
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2022-12-15 18:53:15 +0530

    Finding frequency of words in a sentence like “To be or not to be”

---
+++ MyMapNode.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashTable
+{
+    public class MyMapNode<K, V>
+    {
+        public struct KeyValue<k, v>
+        {
+            public k Key { get; set; }
+            public v Value { get; set; }
+        }
+        private readonly int size;
+        private readonly LinkedList<KeyValue<K, V>>[] items;
+
+        public MyMapNode(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<KeyValue<K, V>>[size];
+        }
+
+        protected int GetPosition(K key)
+        {
+            int hash = key.GetHashCode();
+            int position = hash % size;
+            return Math.Abs(position);
+        }
+
+        public void Add(K key, V value)
+        {
+            int position = GetPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            KeyValue<K, V> item = new KeyValue<K, V>()
+            { Key = key, Value = value };
+            linkedList.AddLast(item);
+            Console.WriteLine(item.Key + " " + item.Value);
+
+        }
+
+        protected LinkedList<KeyValue<K, V>> GetLinkedList(int position)
+        {
+            LinkedList<KeyValue<K, V>> linkedList = items[position];
+            if (linkedList == null)
+            {
+                linkedList = new LinkedList<KeyValue<K, V>>();
+                items[position] = linkedList;
+            }
+            return linkedList;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine(" ");
+            foreach (var linkedList in items)
+            {
+                if (linkedList != null)
+                    foreach (var element in linkedList)
+                    {
+                        string res = element.ToString();
+                        if (res != null)
+                            Console.WriteLine(element.Key + " " + element.Value);
+                    }
+            }
+        }
+
+        public int Frequency(V value)
+        {
+            int count = 0;
+            foreach (var linkedList in items)
+            {
+                if (linkedList != null)
+                {
+                    foreach (var element in linkedList)
+                    {
+                        if (element.Value.Equals(value))
+                            count++;
+                    }
+                }
+            }
+            Console.WriteLine("The frequency of " + value + " is: " + count);
+            return count;
+        }
+    }
+}
