9795c6ed1999b433d421c94369536a7b9f08396a
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2022-12-15 19:42:44 +0530

    [Sangmeshwar Patil]:Creating the binary tree shown in the figure

---
+++ BinaryTree.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchTree
+{
+    class BinaryTree<T> where T : IComparable
+    {
+        public T nodeData { get; set; }
+        public BinaryTree<T> leftTree { get; set; }
+        public BinaryTree<T> rightTree { get; set; }
+        public static int leftCount;
+        public static int rightCount;
+  
+   
+        public BinaryTree(T nodeData)
+        {
+            this.nodeData = nodeData;
+            this.leftTree = null;
+            this.rightTree = null;
+        }
+
+        public void Insert(T item)
+        {
+            T parentValue = this.nodeData;
+            int value = parentValue.CompareTo(item);
+
+            if ((parentValue.CompareTo(item)) > 0)
+            {
+                if (this.leftTree == null)
+                {
+                    this.leftTree = new BinaryTree<T>(item);
+                    Console.WriteLine("Inserting : " + item);
+                }
+                else
+                    this.leftTree.Insert(item);
+            }
+
+            else
+            {
+           
+                if (this.rightTree == null)
+                {
+                    this.rightTree = new BinaryTree<T>(item);
+                    Console.WriteLine("Inserting : " + item);
+                }
+                else
+                {
+                    this.rightTree.Insert(item);
+                }
+
+            }
+        }
+
+        public void Display()
+        {
+            if (this.leftTree != null)
+            {
+                leftCount++;
+            
+                this.leftTree.Display();
+            }
+            Console.WriteLine(this.nodeData.ToString());
+            if (this.rightTree != null)
+            {
+                rightCount++;
+                this.rightTree.Display();
+            }
+
+        }
+
+        public void GetSize()
+        {
+            Console.WriteLine("Size Of BST :" + " " + (1 + leftCount + rightCount));
+        }
+    }
+}
