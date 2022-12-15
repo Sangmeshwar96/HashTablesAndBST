c7d729072395e9420799e072605d9c64317dd5b0
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2022-12-15 18:53:53 +0530

    [Sangmeshwar Patil]:Finding frequency of words in a sentence like “To be or not to be”

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HashTable;
+using System;
 
 namespace HashTables
 {
@@ -7,6 +8,16 @@ namespace HashTables
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to HashTable and Binary search Tree programs");
+            Console.WriteLine(" ");
+
+            MyMapNode<int, string> myMapNode = new MyMapNode<int, string>(5);
+            myMapNode.Add(1, "to");
+            myMapNode.Add(2, "be");
+            myMapNode.Add(3, "or");
+            myMapNode.Add(4, "not");
+            myMapNode.Add(5, "to");
+            myMapNode.Add(6, "be");
+            myMapNode.Frequency("to");
         }
     }
 }
