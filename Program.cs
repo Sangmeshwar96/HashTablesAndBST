f73a6686ca667b200fe25a90d1b242fc122135db
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2022-12-15 19:40:21 +0530

    [Sangmeshwar Patil]:Creating a BST by adding 56 and then adding 30 & 70

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("---Welcome To Binary Search Tree Program---");
+            Console.WriteLine(" ");
+            
+            BinaryTree<int> binaryTree = new BinaryTree<int>(56);
+
+           
+            binaryTree.Insert(30);
+            binaryTree.Insert(70);
+            binaryTree.Insert(22);
+            binaryTree.Insert(40);
+            binaryTree.Insert(11);
+            binaryTree.Insert(18);
+            binaryTree.Insert(3);
+            binaryTree.Insert(60);
+            binaryTree.Insert(95);
+            binaryTree.Insert(65);
+            binaryTree.Insert(63);
+            binaryTree.Insert(67);
+         
+            binaryTree.Display();
+            binaryTree.GetSize();
+        }
+    }
+    
+}
