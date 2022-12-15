0f823ec43c529b2303c018537e17841ac60550ac
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2022-12-15 18:44:23 +0530

    Welcome to HashTable and Binary Search Tree Programs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HashTables
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to HashTable and Binary search Tree programs");
+        }
+    }
+}
